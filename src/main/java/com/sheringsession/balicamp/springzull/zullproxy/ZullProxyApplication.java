fb3b8db581f3680dc9a5e1ac725a787c5fd49213
Gusde Gita <bagus.sugitayasa@gmail.com>
2017-11-26 20:14:13 +0800

    add project zull

---
+++ src/main/java/com/sheringsession/balicamp/springzull/zullproxy/ZullProxyApplication.java
@@ -0,0 +1,14 @@
+package com.sheringsession.balicamp.springzull.zullproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZullProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullProxyApplication.class, args);
+	}
+}
